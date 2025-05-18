b95e4e9ad3db4b547d6a4da7fe5600868fd10d5b
joaohugobotelho <joaohugo202301@gmail.com>
2025-05-18 19:37:40 -0300

    card receita adicionado!

--- src/pages/app/receita/components/ReceitaCard.tsx
+++ src/pages/app/receita/components/ReceitaCard.tsx
@@ -1,5 +1,9 @@
 export default function ReceitaCard() {
     return (
+    <div className="bg-[#1f2937] p-6 rounded-2xl shadow-md">
+        <label className="text-white block mb-2 text-sm font-semibold">
+            Resumo da Receita
+        </label>
         <div className="mb-4">
             <h3 className="text-white text-base font-medium">Ganhos |</h3>
             <p className="text-2xl text-green-400 font-bold">+ R$ 4.400,00</p>
@@ -23,7 +27,7 @@ export default function ReceitaCard() {
                 <p className="text-sm text-gray-400">Ganho Contabilizado</p>
             </div>
         </div>
-
+        </div>
 
     )
 }
